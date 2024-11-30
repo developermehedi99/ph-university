7371926bfd21499ea4629180f14bae3e12d39a6d
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-12-01 00:01:20 +0600

    ph-universite start projects

---
+++ src/app/modules/students/student.schemodel.ts
@@ -0,0 +1,48 @@
+import { model, Schema } from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from './student.interface';
+
+const userNameSchema = new Schema<UserName>({
+  firstName: { type: String, required: true },
+  middleName: { type: String },
+  lastName: { type: String },
+});
+const guardianSchema = new Schema<Guardian>({
+  fatherName: { type: String },
+  fatherOccupation: { type: String },
+  fatherContactNo: { type: String },
+  motherName: { type: String },
+  motherOccupation: { type: String },
+  motherContactNo: { type: String },
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: { type: String },
+  occupation: { type: String },
+  contactNo: { type: String },
+  address: { type: String },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String, required: true },
+  name: userNameSchema,
+  gender: ['male', 'female'],
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String },
+  emergencyContactNo: { type: String },
+  bloogGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  presentAddress: { type: String },
+  permanentAddres: { type: String },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+  profileImg: { type: String },
+  isActive: ['active', 'blocked'],
+});
+
+// model part
+export const studentModel = model<Student>('Student', studentSchema);
